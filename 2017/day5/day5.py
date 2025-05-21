94428d8369ae920f4117e136144f57652278c54e
Robbe.Heirman <robbe.heirman@wolterskluwer.com>
2025-05-21 16:48:44 +0200

    day 5 solution

--- 2017/day5/day5.py
+++ 2017/day5/day5.py
@@ -1,8 +1,27 @@
 from itertools import count
-from typing import Any
+from typing import Callable
 
 import core.python.solution as solution
-from core.python.solution import T
+
+
+def simple_incrementer(pc: int, instructions: list[int]) -> None:
+    instructions[pc] += 1
+
+
+def by_3_incrementer(pc: int, instructions: list[int]) -> None:
+    increment = 1 if instructions[pc] < 3 else -1
+    instructions[pc] += increment
+
+
+def solve(instructions: list[int], instruction_updater: Callable[[int, list[int]], None]) -> int:
+    pc = 0
+    for steps in count(1):
+        instruction = instructions[pc]
+        instruction_updater(pc, instructions)
+        pc += instruction
+        if pc >= len(instructions):
+            return steps
+
 
 class Day5(solution.Solution):
     @classmethod
@@ -11,15 +30,8 @@ class Day5(solution.Solution):
 
     @classmethod
     def solution1(cls, inp: list[int]) -> int:
-        p_c = 0
-        for steps in count(1):
-            p_c += inp[p_c]
-            print(p_c)
-            if p_c >= len(inp):
-                return steps
-            inp[p_c] += 1
-
+        return solve(inp, simple_incrementer)
 
     @classmethod
-    def solution2(cls, inp: T) -> Any:
-        pass
+    def solution2(cls, inp: list[int]) -> int:
+        return solve(inp, by_3_incrementer)
